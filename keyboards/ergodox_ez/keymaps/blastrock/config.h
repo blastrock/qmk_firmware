efc907127104795e79bdd196e44edfba653890a4
Philippe Daouadi <p.daouadi@free.fr>
2018-01-30 22:42:23 +0100

    Add blastrock configuration

---
+++ keyboards/ergodox_ez/keymaps/blastrock/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_USER_H_
+#define CONFIG_USER_H_
+
+#include "../../config.h"
+
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+//#define NO_DEBUG
+//#define DEBUG_ACTION
+//#undef TAPPING_TERM
+//#define TAPPING_TERM 2000
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+//#undef MOUSEKEY_ENABLE
+#undef AUDIO_ENABLE
+#undef RGBLIGHT_ENABLE
+#undef MIDI_ENABLE
+#undef BLUETOOTH_ENABLE
+
+#endif
